e7b5f724cfe1d5c4b8c634d349ae9479303ebfea
Kanzunby <kanzunby@gmail.com>
2023-10-30 14:56:51 +0700

    first commit

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,50 @@
+// import { useState } from "react"
+
+import { useState } from "react";
+
+const Jumbotron = ({fetchData}) => {
+  const [value, setValue] = useState('');
+
+  const handleEnter = e => {
+    if(e.key === 'Enter') {
+      fetchData({params: {title: value}});
+      setValue('')
+    }
+  }
+
+  const hadleButtonSearch = () => {
+    if(value.length) {
+      fetchData({params: {title: value}});
+      setValue('')
+    }
+  }
+
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-center py-20 px-2">
+      <h3 className="text-2xl text-white font-semibold">Search APIs</h3>
+      <div className="flex items-center mt-6 max-w-md mx-auto bg-white  rounded-full">
+        <input 
+        type="search"
+        className="w-full px-4 py-2 rounded-full text-gray-800 focus: outline-none "
+        placeholder="Mau cari apa Bang"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onKeyDown={handleEnter}
+        />
+        <button className="bg-blue-500 hover:bg-blue-600  w-20 h-12 text-white rounded-r-full flex items-center justify-center"
+        onClick={hadleButtonSearch}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbotron
+
+
+
+
